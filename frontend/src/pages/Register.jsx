2d3d8d70d0deeff5e55e2c794b9bbaae1f91531c
masha-a-m <escritoramaree@gmail.com>
2025-07-12 20:51:49 +0800

    added more files

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
-const API_URL = "https://task-manager-1-2nko.onrender.com ";
+const API_URL = "https://task-manager-1-2nko.onrender.com";
 
 document.title = "Sign Up – Clarity";
 
